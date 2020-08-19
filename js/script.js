794e926cda34523ee4eb7a13e071e1ad66228e75
opopop000 <op046428@gmail.com>
2020-08-19 22:53:54 +0900

    EDIT header & ADD section1
    헤더 수정 & 섹션1 - home 파트 작업

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 $('nav > ul > li').mouseenter(function(){
   var subMenu = $(this).find('.sub-menu');
   var subMenuHeight = subMenu.innerHeight();
-  console.log(subMenuHeight)
+  var htmlFont = $('html').css('fontSize').replace('px', '');
+  var htmlFontNum = Number(htmlFont);
+
   $('.sub-menu').hide();
   subMenu.show();
   $('header').stop().animate({
-    height: subMenuHeight + 80 +'px',
+    height: (subMenuHeight / htmlFontNum) + 4 +'rem',
     borderColor: '#0d2d4f',
     borderBottomWidth : '2px'
   }, 400);
@@ -14,7 +16,12 @@ $('nav > ul > li').mouseenter(function(){
 $('nav > ul > li').mouseleave(function(){
   $('.sub-menu').hide();
   $('header').stop().animate({
-    height: 80+'px',
+    height: 4+'rem',
     borderBottomWidth : '1px'
   }, 400);
+});
+
+//lang 버튼
+$('.lang').click(function(){
+  $(this).toggleClass('on');
 });
